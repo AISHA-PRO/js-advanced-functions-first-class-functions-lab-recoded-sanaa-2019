ddc991054da64de3a041be62f59e5cee8fd4d5ec
Aisha Rashed <aisha1rashed@gmail.com>
2019-12-05 20:15:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const returnFirstTwoDrivers = function(arr) {return arr.slice(0,2)};
 const returnLastTwoDrivers = function(arr) {return arr.slice(-2)};
 
 let selectingDrivers =[returnFirstTwoDrivers,returnLastTwoDrivers];
-const createFareMultiplier = function(i){ return function(){i*5}};
+const createFareMultiplier = function(i){ return function(){return i*5}};
  const fareDoubler = function(fareMultiplier){return fareMultiplier*2};
  const fareTripler = function(fareMultiplier){return fareMultiplier*3};
  
